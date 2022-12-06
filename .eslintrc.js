7743208e11da31b6dd0b1b3f6c117ea68a1659fc
Vyacheslav <>
2022-12-07 00:48:10 +0300

    lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
-    tsconfigRootDir : __dirname, 
+    tsconfigRootDir : __dirname,
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
@@ -17,6 +17,176 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
+    'comma-dangle': [
+      'warn',
+      {
+        'arrays': 'always-multiline',
+        'objects': 'always-multiline',
+        'imports': 'always-multiline',
+        'exports': 'always-multiline',
+      }
+    ],
+    'no-undef': 'warn',
+    'no-async-promise-executor': 'warn',
+    'no-inner-declarations': 'warn',
+    'no-extra-boolean-cast': 'warn',
+    'no-prototype-builtins': 'warn',
+    'no-useless-escape': [
+      'warn'
+    ],
+    'no-case-declarations': [
+      'warn'
+    ],
+    'no-constant-condition': [
+      'warn',
+      {
+        'checkLoops': false,
+      }
+    ],
+    'indent': [
+      'warn', 2,
+      {
+        // 1 - отступ case 2 пробела по отношению к switch
+        'SwitchCase': 1,
+      }
+    ],
+    'linebreak-style': [
+      'warn', 'unix',
+    ],
+    'quotes': [
+      'warn',
+      'single'
+    ],
+    'semi': [
+      'warn', 'never',
+    ],
+    'brace-style': [
+      'warn', '1tbs',
+    ],
+    'no-unused-vars': [
+      'warn', 'all',
+    ],
+    'max-len': [
+      'warn',
+      {
+        'code': 120,
+      }
+    ],
+    'key-spacing': [
+      'warn',
+      {
+        'beforeColon': false,
+        'afterColon': true,
+      }
+    ],
+    'array-bracket-newline': [
+      'warn',
+      {
+        'multiline': true,
+        'minItems': 10,
+      }
+    ],
+    'object-property-newline': [
+      'warn',
+      {
+        'allowAllPropertiesOnSameLine': false,
+      }
+    ],
+    'operator-linebreak': [
+      'warn', 'before',
+      {},
+    ],
+    'no-trailing-spaces': [
+      'warn',
+      {
+        'skipBlankLines': true,
+        'ignoreComments': true,
+      },
+    ],
+    'keyword-spacing': [
+      'warn',
+      {
+        'before': true,
+        'after': true,
+      }
+    ],
+    'curly': [
+      'warn',
+      'all'
+    ],
+    'no-irregular-whitespace': [
+      'error',
+      {
+        'skipRegExps': true,
+        'skipTemplates': true,
+        'skipStrings': true,
+      }
+    ],
+
+    'padded-blocks': [
+      'warn',
+      {
+        'blocks': 'never',
+        'classes': 'always',
+        'switches': 'never',
+      },
+    ],
+
+    'space-in-parens': [
+      'warn',
+      'never',
+    ],
+
+    'class-methods-use-this': 'warn',
+
+    'object-curly-newline': [
+      'warn',
+      {
+        'consistent': true,
+        'minProperties': 4,
+      },
+    ],
+
+    'object-curly-spacing': [
+      'warn', 'always'
+    ],
+
+    'no-use-before-define': [
+      'warn',
+      {
+        'functions': false,
+        'classes': false,
+        'variables': false,
+      },
+    ],
+
+    'arrow-parens': [
+      'warn',
+      'as-needed'
+    ],
+
+    'no-useless-constructor': 'off',
+    'no-empty-function': 'warn',
+
+    'camelcase': [
+      'warn',
+      {
+        'properties': 'never',
+      },
+    ],
+
+    'import/prefer-default-export': [
+      'warn',
+    ],
+
+    'no-continue': 'off',
+
+    'max-classes-per-file': ['warn', 1],
+
+    'no-underscore-dangle': 'off',
+
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
